736332530687004a6e504484aa96eaa5dc23a5a6
Vojtěch Tollar <vojtech.tollar@gooddata.com>
2024-10-13 17:20:25 +0200

    test: add LoxTest for automatic Lox script runs
    While this test does not check much, it serves as a simple Lox script runner.
    Any .lox files in the src/test/resources/lox directory
     and sub-directories will be automatically picked up and run.
    
    Chapter: 6 - 8

---
+++ src/test/kotlin/LoxTest.kt
@@ -0,0 +1,32 @@
+package cz.vojtasii.lox
+
+import io.kotest.assertions.throwables.shouldNotThrowAnyUnit
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.WithDataTestName
+import io.kotest.datatest.withData
+import java.nio.file.Path
+import kotlin.io.path.name
+import kotlin.io.path.toPath
+
+internal class LoxTest : FunSpec({
+    val data = LoxTest::class.java.classLoader.getResource("lox")!!
+        .toURI().toPath().toFile()
+        .walkTopDown()
+        .filter { it.isFile && it.extension == "lox" }
+        .map { LoxFileData(it.absolutePath) }
+
+    context("Interpreting Lox code") {
+        withData(data) {
+            println("=== Run ${it.dataTestName()} ===")
+            shouldNotThrowAnyUnit {
+                Lox.main(arrayOf(it.path))
+            }
+        }
+    }
+})
+
+private data class LoxFileData(
+    val path: String,
+) : WithDataTestName {
+    override fun dataTestName(): String = Path.of(path).name
+}
